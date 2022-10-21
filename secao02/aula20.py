beabfcf38283effbcba6991cf44cc01a72206cda
DannyElis89 <dannyelis89@gmail.com>
2022-10-21 18:16:15 -0300

    Fim da aula 20

--- secao02/aula20.py
+++ secao02/aula20.py
@@ -1,11 +1,14 @@
 """
-Condições IF, ELIF e ELSE 
+Condições IF, ELIF e ELSE
 """
 
-idade = 18
+idade = 15
 if idade > 18:
     print('Maior de 18')
+    print(f'Idade = {idade}')
 elif idade < 18:
     print('Menor de 18')
+    print(f'Idade = {idade}')
 else:
     print('18 anos')
+    print(f'Idade = {idade}')
