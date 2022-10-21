23a373b8c27f9ea3b213449b2968b94753d1ee65
DannyElis89 <dannyelis89@gmail.com>
2022-10-21 00:14:23 -0300

    Fim da aula 13

--- secao02/aula13.py
+++ secao02/aula13.py
@@ -3,4 +3,17 @@ print('Hello World!', 123456, 'Danny Elis Simioni')  # Saída: Hello World! 1234
 print('Danny', 'Elis', 'Simioni', sep='-')  # Saída: Danny-Elis-Simioni
 
 print('Danny', 'Elis', sep='-', end='')
-print('Simioni', sep='-', end='')  # Saída: Danny-ElisSimioni
+print('Simioni', sep='-')  # Saída: Danny-ElisSimioni
+
+"""
+imprimir CPF fictício com formatação usando parametros da função print
+"""
+print('Resolução 01', end=': ')
+print('123', end='.')
+print('456', end='.')
+print('789', end='-')
+print('00')
+
+print('Resolução 02', end=': ')
+print('123', '456', '789', sep='.', end='-')
+print('00')
