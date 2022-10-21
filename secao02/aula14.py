b8ec62cc50fc12e5b5b9c8ab16e1c746571e6cea
DannyElis89 <dannyelis89@gmail.com>
2022-10-21 00:31:20 -0300

    Aula 14

---
+++ secao02/aula14.py
@@ -0,0 +1,5 @@
+# Strings em Python
+print('Aspas simples')
+print("Aspas duplas")
+print("Essa é uma string - 'str'")
+print("Essa é uma string - \"str\"")  # Contra-barra antes de um caracter funciona como escape
