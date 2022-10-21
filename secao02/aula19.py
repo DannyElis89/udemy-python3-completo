035487a0d3ad0dc2578cebac48bfa5cfc31d6ea3
DannyElis89 <dannyelis89@gmail.com>
2022-10-21 18:08:58 -0300

    aula 19 e início da aula 20

--- secao02/aula19.py
+++ secao02/aula19.py
@@ -1,6 +1,7 @@
 """
 Entrada de dados do usuário
 """
+
 nome = input('Qual é o seu nome? ')
 idade = int(input('Qual é a sua idade? '))
 
---
+++ secao02/aula20.py
@@ -0,0 +1,11 @@
+"""
+Condições IF, ELIF e ELSE 
+"""
+
+idade = 18
+if idade > 18:
+    print('Maior de 18')
+elif idade < 18:
+    print('Menor de 18')
+else:
+    print('18 anos')
