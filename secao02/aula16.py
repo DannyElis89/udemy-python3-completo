386e196fe8a4f0970ce3497d2ea27d5ebebbaf43
DannyElis89 <dannyelis89@gmail.com>
2022-10-21 15:14:17 -0300

    Aula 16

---
+++ secao02/aula16.py
@@ -0,0 +1,21 @@
+"""
+Operadores aritméticos em Python
++ adição
+- subtração
+* multiplicação
+** exponenciação
+/ divisão
+// divisão inteira
+% módulo (resto da divisão)
+() altera a ordem de precedência
+"""
+
+print('25 + 5 =', 25 + 5)
+print('25 - 5 =', 25 - 5)
+print('25 * 5 =', 25 * 5)
+print('25 ** 5 =', 25 ** 5)
+print('25 / 6 =', 25 / 6)
+print('25 // 6 =', 25 // 6)
+print('25 % 6 =', 25 % 6)
+print('10 + 2 * 5 / 15 =', 10 + 2 * 5 / 15)
+print('(10 + 2) * (5 / 15) =', (10 + 2) * (5 / 15))
